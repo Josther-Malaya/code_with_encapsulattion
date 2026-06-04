17483c42bba0ace865af12408789bdf8e82dfce6
Josther-Malaya <josthermiguelmalaya@gmail.com>
2026-06-05 03:27:22 +0800

    minor changes

--- fan_02.py
+++ fan_02.py
@@ -1,8 +1,9 @@
-class FanTwo:
+from fan_01 import FanOne
+
+class FanTwo(FanOne):
     bg_blue = "\033[1;34m"
-    SLOW = 1
-    MEDIUM = 2
-    FAST = 3
+
     def __init__(self,fan_speed=2,fan_status=False,fan_radius=5, fan_color = "blue"):
         super().__init__(fan_speed,fan_status,fan_radius,fan_color)
-my_fan=FanTwo()
+
+fan_two=FanTwo()
